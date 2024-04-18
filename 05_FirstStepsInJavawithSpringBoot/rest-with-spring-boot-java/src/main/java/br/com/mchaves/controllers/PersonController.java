15864b98989aa570664be025ed11ad651a293bd6
Matheus Chaves <matheuschaves@ciand.com>
2024-04-18 20:58:26 -0300

    ajustado backup

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-java/src/main/java/br/com/mchaves/controllers/PersonController.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-java/src/main/java/br/com/mchaves/controllers/PersonController.java
@@ -1,11 +1,56 @@
 package br.com.mchaves.controllers;
 
+import br.com.mchaves.model.Person;
 import br.com.mchaves.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonService service;
+
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findByAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return service.findByid(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+    @RequestMapping(
+            value = "/{id}",
+            method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
+    }
+
 }
--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-java/src/main/java/br/com/mchaves/services/PersonService.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-java/src/main/java/br/com/mchaves/services/PersonService.java
@@ -1,7 +1,10 @@
 package br.com.mchaves.services;
 
+import br.com.mchaves.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -10,5 +13,50 @@ public class PersonService {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
+    public List<Person> findAll() {
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person");
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one person");
+        return person;
+    }
+    public void delete(String id) {
+        logger.info("Deleting one person");
+    }
+
+
+    public Person findByid(String id) {
+        logger.info("Finding one person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirtName("Matheus");
+        person.setLastName("Chaves");
+        person.setAddress("Rua 3");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirtName("Person Name: " + i);
+        person.setLastName("Last Name: " + i);
+        person.setAddress("In Brazil: " + i);
+        person.setGender("Male");
+        return person;
+    }
 
 }
